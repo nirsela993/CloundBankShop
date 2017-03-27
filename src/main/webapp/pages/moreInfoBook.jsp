b0a45ae31b57a66091cc5437ecf8ad267ac2a45c
jezuzcrist <vfxlord@gmail.com>
2017-03-27 21:45:13 +0300

    cart function + full book view page

---
+++ src/main/webapp/pages/moreInfoBook.jsp
@@ -0,0 +1,50 @@
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>IBM book shop</title>
+	<meta name="description" content="The best book shop ever">
+	<meta name="author" content="Omer Mintz & Nir Sela">
+
+	<link rel="stylesheet" href="../style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<script src="../script/Book.js"></script>
+	<script src="../script/cloudAPI.js"></script>
+	<script src="../script/bookDisplay.js"></script>
+	<script src="../script/loadNavigation.js"></script>
+
+	<body>
+
+
+		<div class="container">
+
+			<div class="row justify-content-md-center" id="pageContent">
+			</div>
+
+			<hr>
+
+			<footer>
+				<p>© Nir Sela and Omer Mintz 2017</p>
+			</footer>
+		</div> 
+
+
+		<script >
+			var bookId = window.location.search.split("=")[1]
+			console.log(bookId)
+			window.loadBooks = ()=>{
+				bookStoreAPI.getBookById(bookId,(book)=>{
+					document.getElementById('pageContent').innerHTML += fullBookCardTemplate(book)
+				})
+			}
+			loadBooks()
+		</script>
+
+	</body>
+	</html>
--- src/main/webapp/script/bookDisplay.js
+++ src/main/webapp/script/bookDisplay.js
@@ -7,16 +7,27 @@ window.listBooks = (books)=>{
 }
 
 window.bookCardTemplate = (book)=>{
-	return (
+	let template =
 	`
 			<div class="card book-card" style="width: 20rem;">
 					<img class="card-img-top book-image" src="${book.coverPicture}" alt="book cover image">
 					<div class="card-block">
 						<h4 class="card-title">${book.title}</h4>
 						<div class="btn-group" role="group" aria-label="Basic example">
-						  <a type="button" class="btn btn-secondary">More Information</a>
-						  <a type="button" class="btn btn-success">Buy for ${book.price}$</a>
-						</div>
+						  <a type="button" class="btn btn-secondary" href="/pages/moreInfoBook.jsp?id=${book._id}">More Info</a>`
+						let currentUser = JSON.parse( localStorage.getItem("user"))
+						if(currentUser != null){
+							let isBookInCart = currentUser.bookIds.includes(book._id)
+							if(isBookInCart){
+								template += `<a type="button" class="btn btn-danger" onclick="removeFromCart('${book._id}')">remove from cart</a>`
+							}else{
+								template += `<a type="button" class="btn btn-success" onclick="addToCart('${book._id}')">Buy for ${book.price}$</a>`
+							}
+						} else {
+							template += `<a type="button" class="btn btn-success" onclick="addToCart('${book._id}')">Buy for ${book.price}$</a>`
+						}
+						template +=
+						`</div>
 						 <ul class="list-group list-group-flush">
 						    <li class="list-group-item">
 						    	<span class="badge badge-default">${book.genre}</span>
@@ -28,5 +39,65 @@ window.bookCardTemplate = (book)=>{
 						<p class="card-text">${book.summary}</p>
 					</div>
 				</div>
-`)
+`
+return template
+}
+
+window.fullBookCardTemplate = (book)=>{
+	let template =
+		`
+			<div class="card book-card" style="width: 50rem;">
+					<img class="card-img-top book-image book-full" src="${book.coverPicture}" alt="book cover image">
+					<div class="card-block">
+						<h4 class="card-title">${book.title}</h4>
+						<div class="btn-group" role="group" aria-label="Basic example">`
+						let currentUser = JSON.parse( localStorage.getItem("user"))
+						if(currentUser != null){
+							let isBookInCart = currentUser.bookIds.includes(book._id)
+							if(isBookInCart){
+								template += `<a type="button" class="btn btn-danger" onclick="removeFromCart('${book._id}')">remove from cart</a>`
+							}else{
+								template += `<a type="button" class="btn btn-info" onclick="addToCart('${book._id}')">add to cart</a>`
+							}
+						} else {
+							template += `<a type="button" class="btn btn-info" onclick="addToCart('${book._id}')">add to cart</a>`
+						}
+						template +=`
+						  <a type="button" class="btn btn-secondary">${book.price}$</a>
+						</div>
+						 <ul class="list-group list-group-flush">
+						    <li class="list-group-item">
+						    	<span class="badge badge-default">${book.genre}</span>
+						    </li>
+						    <li class="list-group-item">${book.author}, pages: ${book.numberOfPages}, rating: ${book.rating}/100</li>
+						    <li class="list-group-item">
+						    </li>
+					    </ul>
+						<p class="card-text">${book.summary}</p>
+					</div>
+				</div>
+`
+return template
+}
+
+window.addToCart = (bookId)=>{
+	let currentUser = JSON.parse( localStorage.getItem("user"))
+	if(currentUser == null){
+		currentUser = { email: prompt("enter your email",""), bookIds:[]}
+	}
+	currentUser.bookIds.push(bookId)
+	localStorage.setItem("user",JSON.stringify( currentUser ) )
+	location.reload()
+}
+
+
+window.removeFromCart = (removeBookId)=>{
+	let currentUser = JSON.parse( localStorage.getItem("user"))
+	if(currentUser == null){
+		return
+		// currentUser = { email: prompt("enter your email",""), bookIds:[]}
+	}
+	currentUser.bookIds = currentUser.bookIds.filter((bookId)=>{ return (bookId != removeBookId)})
+	localStorage.setItem("user",JSON.stringify( currentUser ) )
+	location.reload()
 }
